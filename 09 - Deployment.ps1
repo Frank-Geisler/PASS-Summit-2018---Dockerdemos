a6a6f1dd041be2f2f57a8c8f614c90a6b78cacbe
Tillmann Eitelberg <tillmann@eitelberg.de>
2018-11-08 03:20:21 +0100

    Deployment Script

--- 09 - Deployment.ps1
+++ 09 - Deployment.ps1
@@ -1,18 +1,18 @@
-# Login-AzureRmAccount
-# Set-AzureRmContext -SubscriptionId a83583c4-ea6d-4564-8906-7f646301a498
+# Install-Module dbatools
+Login-AzureRmAccount
+Set-AzureRmContext -SubscriptionId a83583c4-ea6d-4564-8906-7f646301a498
 
 $resourceGroup = "Docker"
 $OsType = "Linux"
 $name = "sql01"
 $location = "westus"
 $envVars = @{ACCEPT_EULA="Y";SA_PASSWORD="!demo54321"}
-
-<#
-
-docker run --name sqlserverdocker -p 1433:1433 -d  frankgeisler/mssql-server-linux-adventureworks2017
-
-#>
-
+$DacpacPath = "C:\Users\Tillmann\OneDrive\Vorträge\PASS Summit 2018\PASS Database\Sample\bin\Debug\Sample.dacpac"
+$DatabaseName = "Sample"
+$PublishProfile = "C:\Users\Tillmann\OneDrive\Vorträge\PASS Summit 2018\PASS Database\Sample\Sample.publish.xml"
+$GenerateDeploymentScript
+$UserName = "sa"
+$Password = "!demo54321" | ConvertTo-SecureString -asPlainText -Force
 
 #-Image microsoft/mssql-server-windows-developer `
 #-Image frankgeisler/mssql-server-linux-adventureworks2017
@@ -35,7 +35,16 @@ while ((Get-AzureRmContainerGroup -ResourceGroupName $resourceGroup -Name $name)
 Write-Host (Get-Date -Format g)
 
 $ipAddress = (Get-AzureRmContainerGroup -ResourceGroupName $resourceGroup -Name $name).IpAddress
-Write-Host $ipAddress
 
-#Remove-AzureRmContainerGroup -ResourceGroupName $resourceGroup -Name $name
+$PublishParams = @{
+	SqlInstance = $ipAddress
+	Path = (Resolve-Path $DacpacPath)
+	Database = $DatabaseName
+	PublishXml = (Resolve-Path $PublishProfile)
+}
 
+if ($UserName) {
+	$PublishParams.Add("SqlCredential", (New-Object System.Management.Automation.PSCredential ($UserName, $Password)))
+}
+
+Publish-DbaDacPackage @PublishParams
